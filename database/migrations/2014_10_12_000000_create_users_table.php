fd55ea053df62a107cc116ae9fc7b4e149ffd27d
Hung Do Viet <viethung097@gmail.com>
2015-10-01 19:21:40 +0700

    first commit

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('id')->unique();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('username')->unique();
+            $table->string('username_slug');
+            $table->string('email')->unique();
+            $table->string('comfirmation_code');
+            $table->string('comfirmed')->default(false);
+            $table->string('password', 60);
+            $table->json('settings');
+            $table->boolean('admin')->default(false);
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('users');
+    }
+}
